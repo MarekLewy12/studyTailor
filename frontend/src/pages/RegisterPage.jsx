f1647ddba65d667545c47fcce3315cf15a2393be
MarekLewy12 <lewarmarko@gmail.com>
2025-01-02 15:48:46 +0100

    improve the look of register page

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -39,94 +39,113 @@ const RegisterPage = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center relative">
-      <div className="container mx-auto py-12 px-4 max-w-md">
-        <AnimatedSection>
-          <div className="text-center mb-8">
+      <div className="container mx-auto py-12 px-4 max-w-4xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="flex flex-col justify-center items-center p-6 rounded-lg shadow-md border-2 border-amber-500">
+            <h2 className="text-2xl font-bold text-orange-600 mb-4">
+              Witamy w StudyTailor
+            </h2>
+            <p className="text-gray-700 mb-4 text-center">
+              Zarejestruj się, aby uzyskać dostęp do wszystkich funkcji naszej
+              aplikacji.
+            </p>
+            <ul className="list-disc list-inside text-gray-700">
+              <li>Automatyzacja harmonogramów</li>
+              <li>Dostęp do utworzonych harmonogramów</li>
+              <li>Przyjazny interfejs</li>
+              <li>I inne!</li>
+            </ul>
             <img
               src="/study_logo_orange_no_text.png"
               alt="Logo aplikacji"
-              className="mx-auto h-32 w-auto mb-4 mt-5"
+              className="mt-6 h-32 w-auto"
             />
-            <h1 className="text-3xl md:text-4xl font-bold text-orange-600">
-              Witaj w studyTailor
-            </h1>
-            <h2 className="mt-4 text-lg text-gray-600">
-              Zarejestruj swoje konto i zacznij planować naukę już teraz!
-            </h2>
           </div>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="login"
-              >
-                Login
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="login"
-                type="login"
-                placeholder="Login"
-                value={login}
-                onChange={(e) => setLogin(e.target.value)}
-              />
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="password"
-              >
-                Hasło
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="Hasło"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Link
-                to="/forgot-password"
-                className="text-sm text-orange-500 hover:text-orange-700"
-              >
-                Zapomniałeś hasła?
-              </Link>
-            </div>
-            <div className="mb-4">
-              <label
-                className="block text-blue-700 text-sm font-bold mb-2"
-                htmlFor="albumNumber"
-              >
-                Numer albumu
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="albumNumber"
-                type="text"
-                placeholder="Numer albumu"
-                value={albumNumber}
-                onChange={(e) => setAlbumNumber(e.target.value)}
-              />
-            </div>
-            <div className="text-center">
-              <button
-                type="submit"
-                className={`
-                text-white font-semibold py-2 px-6 rounded-lg transition duration-300
-                ${
-                  isFormValid
-                    ? "bg-orange-600 hover:bg-orange-800"
-                    : "bg-orange-600 opacity-50 cursor-not-allowed"
-                }
-              `}
-                disabled={!isFormValid}
-              >
-                Zarejestruj się
-              </button>
-            </div>
-          </form>
-        </AnimatedSection>
+          <div>
+            <AnimatedSection>
+              <div className="text-center mb-8">
+                <h1 className="text-3xl md:text-4xl font-bold text-orange-600">
+                  Zarejestruj się
+                </h1>
+                <h2 className="mt-4 text-lg text-gray-600">
+                  Zarejestruj swoje konto i zacznij planować naukę już teraz!
+                </h2>
+              </div>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="login"
+                  >
+                    Login
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="login"
+                    type="login"
+                    placeholder="Login"
+                    value={login}
+                    onChange={(e) => setLogin(e.target.value)}
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="password"
+                  >
+                    Hasło
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password"
+                    type="password"
+                    placeholder="Hasło"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Link
+                    to="/forgot-password"
+                    className="text-sm text-orange-500 hover:text-orange-700"
+                  >
+                    Zapomniałeś hasła?
+                  </Link>
+                </div>
+                <div className="mb-4">
+                  <label
+                    className="block text-gray-700 text-sm font-bold mb-2"
+                    htmlFor="albumNumber"
+                  >
+                    Numer albumu
+                  </label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="albumNumber"
+                    type="text"
+                    placeholder="Numer albumu"
+                    value={albumNumber}
+                    onChange={(e) => setAlbumNumber(e.target.value)}
+                  />
+                </div>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className={`
+                    text-white font-semibold py-2 px-6 rounded-lg transition duration-300
+                    ${
+                      isFormValid
+                        ? "bg-orange-600 hover:bg-orange-800"
+                        : "bg-orange-600 opacity-50 cursor-not-allowed"
+                    }
+                  `}
+                    disabled={!isFormValid}
+                  >
+                    Zarejestruj się
+                  </button>
+                </div>
+              </form>
+            </AnimatedSection>
+          </div>
+        </div>
       </div>
       {/* TODO: Atrybucja */}
       <img
