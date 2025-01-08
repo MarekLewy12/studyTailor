af3682fc2c8459a1f1362a1598c4b75d5d9fb003
MarekLewy12 <lewarmarko@gmail.com>
2025-01-08 12:10:26 +0100

    fix createsuperuser bug

--- backend/studyPlannerAPI/models.py
+++ backend/studyPlannerAPI/models.py
@@ -1,5 +1,22 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+
+class CustomUserManager(UserManager):
+    def create_superuser(
+        self, username, email = ..., password = ..., **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        # Usuwanie numeru albumu
+        extra_fields.pop("album_number", None)
+
+        return self._create_user(username, email, password, **extra_fields)
 
 
 class ModelRequest(models.Model):
@@ -14,4 +31,14 @@ class ModelRequest(models.Model):
 
 # Model studencki z numerem albumu
 class CustomUser(AbstractUser):
-    album_number = models.CharField(max_length=5, unique=True)
+    album_number = models.CharField(max_length=5, unique=True, null=True, blank=True)
+
+    # Podmiana domyślnego managera na CustomUserManager
+    objects = CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.album_number = None
+        elif not self.album_number:
+            raise ValueError("Album number is required for non-superusers.")
+        super().save(*args, **kwargs)
