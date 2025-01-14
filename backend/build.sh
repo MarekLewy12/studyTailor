47fcbfda37c50f58927658cb03de457b0aa60a0c
MarekLewy12 <lewarmarko@gmail.com>
2025-01-14 20:52:22 +0100

    fix build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,13 @@
 # Instalowanie zależności, zbieranie statycznych plików i migracja bazy danych
 echo "Building the project..."
 python -m pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+echo "Making migrations..."
+cd backend  # Przechodzimy do katalogu z manage.py
+python manage.py makemigrations
 python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+echo "Build completed!"
