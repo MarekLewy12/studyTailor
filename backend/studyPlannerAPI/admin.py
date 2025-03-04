8e253463883fdfe71ea93520f8dfe7482b3eec88
MarekLewy12 <lewarmarko@gmail.com>
2025-03-04 18:25:59 +0100

    add new data for db for admin

--- backend/studyPlannerAPI/admin.py
+++ backend/studyPlannerAPI/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, ModelRequest, Subject, Material, StudySession
 
 from .models import ModelRequest
 admin.site.register(ModelRequest)
@@ -21,4 +21,15 @@ class CustomUserAdmin(UserAdmin):
         ('Dane osobowe', {'fields': ('album_number',)}),
         ('Uprawnienia', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
     )
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lesson_form', 'start_datetime', 'end_datetime', 'is_mastered', 'user')
+    list_filter = ('user', 'lesson_form', 'is_mastered')
+    search_fields = ('name', 'user__username')
+    ordering = ('start_datetime',)
+
+
+admin.site.register(Material)
+admin.site.register(StudySession)
 
