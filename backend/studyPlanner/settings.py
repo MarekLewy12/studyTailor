b1fa38b741868128f678b985d74003889ac5012f
MarekLewy12 <lewarmarko@gmail.com>
2025-03-14 18:43:52 +0100

    add AWS Location

--- backend/studyPlanner/settings.py
+++ backend/studyPlanner/settings.py
@@ -214,5 +214,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 
+AWS_LOCATION = 'media'
+
 # Konfiguracja przechowywania plików
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
